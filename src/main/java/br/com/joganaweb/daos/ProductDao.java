573aa2f260a50eed1e6bdbb53f39aebbf84f80a3
PatriciaDurand <durand_patricia_@hotmail.com>
2016-02-16 22:17:06 -0200

    ...

---
+++ src/main/java/br/com/joganaweb/daos/ProductDao.java
@@ -0,0 +1,39 @@
+package br.com.joganaweb.daos;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import br.com.joganaweb.models.Product;
+
+public class ProductDao {
+
+    @PersistenceContext
+    private EntityManager manager;
+
+    public List<Product> all()
+    {
+        return manager.createQuery("select p from Product p", Product.class).getResultList();
+    }
+
+    public void save(Product product)
+    {
+        manager.persist(product);
+    }
+
+    public Product findById(Integer id)
+    {
+        return manager.find(Product.class, id);
+    }
+
+    public void remove(Product product)
+    {
+        manager.remove(product);
+    }
+
+    public void update(Product product)
+    {
+        manager.merge(product);
+    }
+
+}
